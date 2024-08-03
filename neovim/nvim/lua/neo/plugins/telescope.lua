45556b3351f6ad05a095cb668458506fb7c248f4
Abdennour Zahaf <zfnori@gmail.com>
2024-08-03 02:49:06 +0100

    feat(nvim): small telescope keymaps improvements

--- neovim/nvim/lua/neo/plugins/telescope.lua
+++ neovim/nvim/lua/neo/plugins/telescope.lua
@@ -66,27 +66,32 @@ return {
     })
 
     vim.keymap.set("n", "gt", builtin.lsp_type_definitions, { desc = "Show LSP type definitions" })
-    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Word under cursor" })
-    vim.keymap.set("n", "<leader>sg", function()
-      builtin.live_grep({ additional_args = { "--smart-case" } })
-    end, { desc = "Telescope: Grep (smart case)" })
     vim.keymap.set("n", "<leader>sG", builtin.live_grep, { desc = "Telescope: Grep (case sensitive)" })
     vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Telescope: Git Files" })
     vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "Telescope: Document Symbols" })
     vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find, { desc = "Telescope: Search Buffer" })
     vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Telescope: Resume" })
     vim.keymap.set("n", "<leader>r", builtin.oldfiles, { desc = "Telescope: Recent Files" })
-    -- vim.keymap.set("n", "<leader>o", builtin.buffers, { desc = "Telescope: Buffers" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Telescope: Key Maps" })
     vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "Telescope: Commands" })
     vim.keymap.set("n", "<leader>sj", builtin.git_commits, { desc = "Telescope: Commits" })
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope: Help Tags" })
-    vim.keymap.set("n", "<leader>sd", function()
-      builtin.diagnostics({ bufnr = 0 })
-    end, { desc = "Show buffer diagnostics" })
     vim.keymap.set("n", "<leader>sD", builtin.diagnostics, { desc = "Show workspace diagnostics" })
+
     vim.keymap.set("n", "<leader>sn", function()
       builtin.find_files({ cwd = vim.env.HOME .. "/Projects/dotfiles" })
     end, { desc = "Telescope: Dotfiles" })
+
+    vim.keymap.set("n", "<leader>sw", function()
+      builtin.live_grep({ additional_args = { "--smart-case" }, default_text = vim.fn.expand("<cword>") })
+    end, { desc = "Telescope: Grep word under cursor (smart case)" })
+
+    vim.keymap.set("n", "<leader>sg", function()
+      builtin.live_grep({ additional_args = { "--smart-case" } })
+    end, { desc = "Telescope: Grep (smart case)" })
+
+    vim.keymap.set("n", "<leader>sd", function()
+      builtin.diagnostics({ bufnr = 0 })
+    end, { desc = "Show buffer diagnostics" })
   end,
 }
