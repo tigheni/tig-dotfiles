259c0816cd4e92a9c17c2c1b0274379a177b800b
Abdennour Zahaf <zfnori@gmail.com>
2024-10-30 15:28:36 +0100

    feat(nvim): add treesitter context keymap

--- neovim/nvim/lua/neo/plugins/nvim-treesitter-context.lua
+++ neovim/nvim/lua/neo/plugins/nvim-treesitter-context.lua
@@ -6,7 +6,10 @@ return {
     tc.setup()
     vim.keymap.set("n", "<leader>t", "<cmd>TSContextToggle<CR>", { desc = "Toggle TS Context" })
     vim.keymap.set("n", "<leader>z", function()
+      tc.go_to_context(1)
+    end, { silent = true, desc = "Jump to context" })
+    vim.keymap.set("n", "<leader>Z", function()
       tc.go_to_context(10)
-    end, { silent = true, desc = "Jump to context (upwards)" })
+    end, { silent = true, desc = "Jump to context (furthest)" })
   end,
 }
