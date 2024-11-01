9e36e0141df08f84a1af55266d9e85ba4a9472fb
Abdennour Zahaf <zfnori@gmail.com>
2024-11-01 23:18:09 +0100

    feat(nvim): add copy blame commit keymap

--- neovim/nvim/lua/neo/plugins/git-messenger.lua
+++ neovim/nvim/lua/neo/plugins/git-messenger.lua
@@ -6,6 +6,9 @@ return {
     git_blame.setup({ display_virtual_text = false, message_template = "<sha>" })
 
     vim.keymap.set("n", "<leader>go", "<cmd>GitBlameOpenCommitURL<cr>", { desc = "Open commit URL" })
+    vim.keymap.set("n", "<leader>gy", function()
+      vim.fn.setreg("+", git_blame.get_current_blame_text())
+    end, { desc = "Copy blame commit" })
     vim.keymap.set("n", "<leader>gj", function()
       vim.cmd("!git checkout " .. vim.print(git_blame.get_current_blame_text()) .. "~1")
     end, { desc = "Checkout blame commit" })
