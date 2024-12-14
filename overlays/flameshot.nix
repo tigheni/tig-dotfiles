4af40523a8df02f6904039f2847d5bb33eb645be
Abdennour Zahaf <zfnori@gmail.com>
2024-12-14 20:17:01 +0100

    chore: remove flameshot overlay pin

--- overlays/flameshot.nix
+++ overlays/flameshot.nix
@@ -1,12 +1,5 @@
 final: prev: {
   flameshot = prev.flameshot.overrideAttrs (prev: {
-    src = final.fetchFromGitHub {
-      owner = "flameshot-org";
-      repo = "flameshot";
-      rev = "14a136777cd82ab70f42c13b4bc9418c756d91d2";
-      hash = "sha256-xM99adstwfOOaeecKyWQU3yY0p65pQyFgoz7WJNra98=";
-    };
-
     nativeBuildInputs = prev.nativeBuildInputs ++ [final.libsForQt5.kguiaddons];
 
     cmakeFlags = [
