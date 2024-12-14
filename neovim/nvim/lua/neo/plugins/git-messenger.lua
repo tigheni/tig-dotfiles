792ce01beee9d5b573491ad4d2b2908e21cf5c04
Abdennour Zahaf <zfnori@gmail.com>
2024-12-14 20:07:24 +0100

    feat(nvim): add yank file url keymap

--- neovim/nvim/lua/neo/plugins/git-messenger.lua
+++ neovim/nvim/lua/neo/plugins/git-messenger.lua
@@ -6,11 +6,12 @@ return {
     git_blame.setup({ display_virtual_text = false, message_template = "<sha>" })
 
     vim.keymap.set("n", "<leader>go", "<cmd>GitBlameOpenCommitURL<cr>", { desc = "Open commit URL" })
-    vim.keymap.set("n", "<leader>gy", function()
+    vim.keymap.set("x", "<leader>gyf", ":GitBlameCopyFileURL<cr>", { desc = "Copy file URL" })
+    vim.keymap.set("n", "<leader>gyc", function()
       vim.fn.setreg("+", git_blame.get_current_blame_text())
     end, { desc = "Copy blame commit" })
     vim.keymap.set("n", "<leader>gj", function()
-      vim.cmd("!git checkout " .. vim.print(git_blame.get_current_blame_text()) .. "~1")
+      vim.cmd("!git checkout " .. git_blame.get_current_blame_text() .. "~1")
     end, { desc = "Checkout blame commit" })
   end,
 }
