762bf1c897d53276fec452803f12dfa631d9bf3c
Abdennour Zahaf <zfnori@gmail.com>
2024-12-14 19:42:56 +0100

    fix: patch vlc for wayland

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,18 @@
     isNormalUser = true;
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
+      (pkgs.symlinkJoin {
+        name = "vlc";
+        paths = [pkgs.vlc];
+        buildInputs = [pkgs.makeWrapper];
+        postBuild = ''
+          wrapProgram $out/bin/vlc \
+            --unset DISPLAY
+          mv $out/share/applications/vlc.desktop{,.orig}
+          substitute $out/share/applications/vlc.desktop{.orig,} \
+            --replace-fail Exec=${pkgs.vlc}/bin/vlc Exec=$out/bin/vlc
+        '';
+      })
       brave
       spotify
       gh
@@ -44,7 +56,6 @@
       fd
       jq
       webtorrent_desktop
-      vlc
       ytarchive
       ffmpeg
       zoxide
